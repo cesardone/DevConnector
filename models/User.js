ead199088f9359fd44a6a37e2d75d6778fb212a8
cesar <CesarD>
2019-12-03 13:46:01 -0500

    Authentication added

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -18,6 +19,7 @@ const UserSchema = new mongoose.Schema({
         type: String
     },
     date: {
+        type: Date,
         default: Date.now
     }
 });
